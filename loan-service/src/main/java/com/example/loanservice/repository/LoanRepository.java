ad8f9baf9723e235fea167b1cd9baa4919ab57ed
Carolina Atampis <carolinanatampis@gmail.com>
2022-07-14 11:34:33 -0300

    practicas

---
+++ loan-service/src/main/java/com/example/loanservice/repository/LoanRepository.java
@@ -0,0 +1,9 @@
+package com.example.loanservice.repository;
+
+import com.example.loanservice.entity.Loan;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface LoanRepository extends JpaRepository<Loan, Long> {
+
+}
